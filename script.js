8bcdabd7c2cce3ac49b5cdd805b3532896a8816b
Gorka <doctor.scissor@gmail.com>
2015-11-03 10:55:58 +0500

    solution 2

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 function solution1(A, B, C) {
+
     if (A < B && C > 0) {
-        return Math.ceil((B - A) / C);
+        return ('The minimal number of days: ' + Math.ceil((B - A) / C));
     }
+
     return false;
 };
 
+function solution2(A, B) {
+    var maxXOR = 0,
+        curXOR, C;
+
+    while (A < B) {
+        C = A + 1;
+        while (C <= B) {
+            curXOR =  A^C;
+            if (curXOR > maxXOR) {
+                maxXOR = curXOR;
+            }
+            C++;
+        }
+        A++;
+    }
+
+    return ('The maximal *XOR* value for two numbers from [A, B]: ' + maxXOR);
+};
